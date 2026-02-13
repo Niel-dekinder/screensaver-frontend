47958a7fcb61e9f29a7dae80c6ae3e283073049a
Niel Cami <niel@dekinder.be>
2026-02-13 14:13:54 +0100

    init commit

---
+++ live-screensaver/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  let messages = [
+    { type: "logistics", text: "📦 new Milwaukee arrived" },
+    { type: "delivery", text: "🚚 Bosch delayed - 22 feb" },
+    { type: "promo", text: "🟢 New Promo tomorrow" },
+  ];
+
+</script>
+
+<div class="container">
+  {#each messages as message (message)}
+    <div class="message">
+      {message.text}
+    </div>
+  {/each}
+</div>
+
+<style>
+  :global( body) {
+    margin: 0;
+    background: black;
+  }
+
+  .container {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .message {
+    color: white;
+    font-size: 40px;
+    font-family: Arial, sans-serif;
+    text-align: center;
+  }
+</style>
