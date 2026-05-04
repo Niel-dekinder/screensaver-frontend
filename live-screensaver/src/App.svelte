b98c371b7ec9d4a1f0802b42761743a727bfa838
Niel Cami <niel@dekinder.be>
2026-05-04 14:59:38 +0200

    add exclusion list

--- live-screensaver/src/App.svelte
+++ live-screensaver/src/App.svelte
@@ -58,8 +58,11 @@
 
   function get_expiration_date(days: string) {
     const date = new Date();
-    date.setUTCDate(date.getUTCDate() + Number(days));
-    return date.toISOString().slice(0, 19).replace("T", " ");
+    date.setDate(date.getDate() + Number(days));
+    const y = date.getFullYear();
+    const m = String(date.getMonth() + 1).padStart(2, "0");
+    const d = String(date.getDate()).padStart(2, "0");
+    return `${y}-${m}-${d} 23:59:59`;
   }
 
   function getEmoji(type: string): string {
